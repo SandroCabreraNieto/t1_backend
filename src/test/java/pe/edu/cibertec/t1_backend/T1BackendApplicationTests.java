3ea5cf21868fd14de25db1e16fd6f88336eaece7
Sandro Cabrera <cabrerasandro682@gmail.com>
2024-09-25 22:20:05 -0500

    Carga inicial backend T1

---
+++ src/test/java/pe/edu/cibertec/t1_backend/T1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
